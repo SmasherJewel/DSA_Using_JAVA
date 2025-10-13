cccb029d4f7f490aaf67edde213b34cdcee91c28
smasher <ffadda2020@gmail.com>
2025-10-13 23:50:10 +0530

    feat: add solution for LeftTriangle_StarPattern

---
+++ Pattern_Problems/Q1_LeftTriangle_StarPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q1_LeftTriangle_StarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of Rows: ");
+        int n = sc.nextInt();
+
+        pattern1(n);
+    }
+
+    static void pattern1(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
