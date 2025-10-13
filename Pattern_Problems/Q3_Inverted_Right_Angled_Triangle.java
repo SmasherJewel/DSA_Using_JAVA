36b91a46b22f229e56f5b1e6e0f7b41fb268120f
smasher <ffadda2020@gmail.com>
2025-10-14 00:07:07 +0530

    fate: add solution for Inverted Right-Angled Triangle.

---
+++ Pattern_Problems/Q3_Inverted_Right_Angled_Triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q3_Inverted_Right_Angled_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of Rows: ");
+        int n = sc.nextInt();
+
+        pattern3(n);
+    }
+
+    static void pattern3(int n){
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
