8dfc21f13f5d3df0fdc5979040bb499c15314c2e
smasher <ffadda2020@gmail.com>
2025-10-14 00:01:31 +0530

    fate: add solution for Solid Square Pattern

---
+++ Pattern_Problems/Q2_Solid_Square_Pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q2_Solid_Square_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of Rows: ");
+        int n = sc.nextInt();
+
+        pattern2(n);
+    }
+
+    static void pattern2(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
