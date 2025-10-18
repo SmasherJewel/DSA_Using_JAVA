63499e45dbcea77d53e318fba140d3869109c34a
smasher <ffadda2020@gmail.com>
2025-10-18 11:48:31 +0530

    fate: 1 new pattern added

---
+++ Pattern_Problems/Q12_RightPascal_TriangleStar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q12_RightPascal_TriangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of Rows: ");
+        int n = sc.nextInt();
+
+        pattern12(n);
+    }
+    static void pattern12(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Reverse Portion
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
