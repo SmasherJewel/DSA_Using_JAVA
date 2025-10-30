9117925afe3c96fe4244436dcc4172a316e4aeca
smasher <ffadda2020@gmail.com>
2025-10-30 22:55:46 +0530

    fate:Find Sum of Digit of an Integer Number Programe added

---
+++ Basic_Java_Problems/src/Q2_SumOfDigits.java
@@ -0,0 +1,28 @@
+/*
+Question:
+Write a Java program to find and display the sum of digits of a given number.
+
+The program should:
+1. Ask the user to enter an integer number.
+2. Extract each digit from the number and calculate the sum of all digits.
+3. Display the total sum of the digits.
+*/
+
+// Optimized Solution
+
+import java.util.Scanner;
+
+public class Q2_SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to find the sum of its digits: ");
+        int num = sc.nextInt();
+        int digitSum = 0;
+
+        while (num > 0){
+            digitSum += num % 10;
+            num /= 10;
+        }
+        System.out.println("The Sum of Digits is: " + digitSum);
+    }
+}
