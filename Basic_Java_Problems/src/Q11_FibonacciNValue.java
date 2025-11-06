6b66d55b461edd8f10c280c677e899a639082e37
smasher <ffadda2020@gmail.com>
2025-11-06 23:20:21 +0530

    fate: add optimized Fibonacci series up to N value using while loop

--- Basic_Java_Problems/src/Q11_FibonacciNValue.java
+++ Basic_Java_Problems/src/Q11_FibonacciNValue.java
@@ -1,5 +1,39 @@
+/*
+Question:
+Write a Java program to print the Fibonacci series up to a given number (N value).
+
+The program should:
+1. Ask the user to enter a number (N).
+2. Use a for loop to generate and display all Fibonacci numbers
+   less than or equal to N.
+3. Display all terms starting from 0 and 1 up to the specified limit.
+
+Example:
+Input: 10
+Output:
+Fibonacci Series up to 10:
+0 1 1 2 3 5 8
+*/
+
+// Optimized Solution
+
+import java.util.Scanner;
+
 public class Q11_FibonacciNValue {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the limit (N value) for Fibonacci series: ");
+        int limit = sc.nextInt();
+        sc.close();
 
+        int x = 0, y = 1, z = 0;
+        System.out.println("\nFibonacci Series up to " + limit + ":");
+        while (x <= limit){
+            System.out.print(x + " ");
+            z = x + y;
+            x = y;
+            y = z;
+        }
+        System.out.println();
     }
 }
